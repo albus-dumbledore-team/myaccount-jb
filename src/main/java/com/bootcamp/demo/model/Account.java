365b3c3ec05b7a099b3379f284d3214eb33d2c43
cosminflorin1 <florincosminmateescu@gmail.com>
2021-11-13 18:48:31 +0200

    DateOfBirth is now a Date

---
+++ src/main/java/com/bootcamp/demo/model/Account.java
@@ -0,0 +1,85 @@
+package com.bootcamp.demo.model;
+
+
+import java.util.Date;
+
+public class Account {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    Date dateOfBirth;
+
+    public Account(String name, String email, String username, String password, String phoneNumber, String address,  Date dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+
+    }
+
+    public Account(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public  Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth( Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
