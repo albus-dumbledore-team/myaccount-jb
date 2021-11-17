f677637d2abdedc391e0744b196c587ec04d8cf7
cosmin-195 <mcie2958@scs.ubbcluj.ro>
2021-11-17 17:12:24 +0200

    removed print

--- src/main/java/com/bootcamp/demo/business/AccountService.java
+++ src/main/java/com/bootcamp/demo/business/AccountService.java
@@ -14,10 +14,19 @@ public class AccountService implements Service<Account> {
     AbstractRepository<Account> repository;
     Encryptor encryptor;
 
+    @Autowired
+    public void setRepository(AbstractRepository<Account> repository) {
+        this.repository = repository;
+    }
+
+    @Autowired
+    public void setEncryptor(Encryptor encryptor) {
+        this.encryptor = encryptor;
+    }
+
     public String add(final Account account) throws ServiceException {
         try {
             account.setPassword(encryptor.encryptSHA256(account.getPassword()));
-            System.out.println(account.getPassword());
             return repository.add(account);
         } catch (ExecutionException | InterruptedException exception) {
             throw new ServiceException(exception.getMessage());
