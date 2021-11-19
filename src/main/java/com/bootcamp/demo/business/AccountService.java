1929fe152fe4a82b3b9a147c99e2399593278823
Vultur Ana <vulturana@MacBook-Pro-Vultur.local>
2021-11-19 11:52:43 +0200

    #ValidationService

--- src/main/java/com/bootcamp/demo/business/AccountService.java
+++ src/main/java/com/bootcamp/demo/business/AccountService.java
@@ -5,6 +5,12 @@ import com.bootcamp.demo.data_access.AbstractRepository;
 import com.bootcamp.demo.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -22,6 +28,41 @@ public class AccountService implements Service<Account> {
     @Autowired
     public void setEncryptor(Encryptor encryptor) {
         this.encryptor = encryptor;
+    }
+
+    public boolean accountValidation(Account account){
+        if(account.getName().isEmpty() || account.getPassword().isEmpty() || account.getEmail().isEmpty() || account.getUsername().isEmpty() || account.getAddress().isEmpty() || account.getPhoneNumber().isEmpty() || account.getDateOfBirth().isEmpty())
+            return false;
+
+        String expression = "^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$";
+        if(!account.getName().matches(expression))
+            return false;
+
+        expression = "[a-zA-Z0-9]*$";
+        if(!account.getUsername().matches(expression))
+            return false;
+
+        if(account.getPhoneNumber().length() != 10)
+            return false;
+
+        for(char c: account.getPhoneNumber().toCharArray()){
+            if(!Character.isDigit(c))
+                return false;
+        }
+
+        String pattern = "MM-dd-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(account.getDateOfBirth());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        Period period = Period.between(localDate, LocalDate.now());
+
+        return (period.getYears() > 18);
+
     }
 
     public String add(final Account account) throws ServiceException {
