bf057d8693fec956ff2a73b3714ee65297883224
Vultur Ana <vulturana@MacBook-Pro-Vultur.local>
2021-11-21 23:30:37 +0200

    Added validations for account

--- src/main/java/com/bootcamp/demo/business/AccountService.java
+++ src/main/java/com/bootcamp/demo/business/AccountService.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
@@ -30,45 +29,60 @@ public class AccountService implements Service<Account> {
         this.encryptor = encryptor;
     }
 
-    public boolean accountValidation(Account account){
+    public String accountValidation(Account account) throws ServiceException {
+
+        StringBuilder stringBuilder = new StringBuilder();
         if(account.getName().isEmpty() || account.getPassword().isEmpty() || account.getEmail().isEmpty() || account.getUsername().isEmpty() || account.getAddress().isEmpty() || account.getPhoneNumber().isEmpty() || account.getDateOfBirth().isEmpty())
-            return false;
+            stringBuilder.append("Empty fields");
 
         String expression = "^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$";
         if(!account.getName().matches(expression))
-            return false;
+            stringBuilder.append("Wrong name format: must contain only letters and must be \"FirstName LastName\"  \n");
 
         expression = "[a-zA-Z0-9]*$";
         if(!account.getUsername().matches(expression))
-            return false;
+            stringBuilder.append("Wrong username format: must contain only digits and letter\n");
 
         if(account.getPhoneNumber().length() != 10)
-            return false;
-
-        for(char c: account.getPhoneNumber().toCharArray()){
-            if(!Character.isDigit(c))
-                return false;
+            stringBuilder.append("Wrong phoneNumber format: must have 10 digits\n");
+        else {
+            for (char c : account.getPhoneNumber().toCharArray()) {
+                if (!Character.isDigit(c))
+                    stringBuilder.append("Wrong phoneNumber format: must contain only digits\n");
+            }
         }
 
-        String pattern = "MM-dd-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        Date date = null;
-        try {
-            date = simpleDateFormat.parse(account.getDateOfBirth());
-        } catch (ParseException e) {
-            e.printStackTrace();
+        if(account.getDateOfBirth().length() != 10)
+            stringBuilder.append("Wrong dateOfBirth format: must be MM-dd-yyyy\n");
+        else {
+            String pattern = "MM-dd-yyyy";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            try {
+                Date date = simpleDateFormat.parse(account.getDateOfBirth());
+                LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                Period period = Period.between(localDate, LocalDate.now());
+                if (period.getYears() < 18)
+                    stringBuilder.append("Minimum age must be 18\n");
+            } catch (ParseException e) {
+                stringBuilder.append("Wrong dateOfBirth format: must be MM-dd-yyyy\n");
+                e.printStackTrace();
+            }
         }
-        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        Period period = Period.between(localDate, LocalDate.now());
 
-        return (period.getYears() > 18);
-
+        if(stringBuilder.length() != 0)
+            throw new ServiceException(stringBuilder.toString());
+        else {
+            return ("OK");
+        }
     }
 
     public String add(final Account account) throws ServiceException {
         try {
             account.setPassword(encryptor.encryptSHA256(account.getPassword()));
-            return repository.add(account);
+            if(accountValidation(account).equals("OK"))
+                return repository.add(account);
+            else
+                throw new ServiceException(accountValidation(account));
         } catch (ExecutionException | InterruptedException exception) {
             throw new ServiceException(exception.getMessage());
         }
